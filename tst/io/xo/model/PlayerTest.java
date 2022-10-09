78bd2b0151838450aff01ea79c195daba8ee1f9c
Alex <Vaas19.96@yandex.ru>
2022-10-10 00:05:43 +0300

    PlayerTest created

---
+++ tst/io/xo/model/PlayerTest.java
@@ -0,0 +1,34 @@
+package io.xo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() throws Exception{
+
+        final String inputValue = "Alex";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() throws Exception{
+
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = player.getFigure();
+
+        assertEquals(expectedValue, actualValue);
+    }
+}
