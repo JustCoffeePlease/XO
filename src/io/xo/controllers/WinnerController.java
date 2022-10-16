517c9752b4f382f5f75007371029b9e568ac380b
Alex <Vaas19.96@yandex.ru>
2022-10-16 10:50:04 +0300

    WinnerController - bad practice

--- src/io/xo/controllers/WinnerController.java
+++ src/io/xo/controllers/WinnerController.java
@@ -11,48 +11,26 @@ public class WinnerController {
     public Figure getWinner(final Field field) {
 
         try {
-
             for (int i = 0; i < 3; i++) {
-                if (check(field, new Point(i, 0), p -> new Point(p.x + 1, p.y))) // Пример упрощенной реализации
+                if (check(field, new Point(i, 0), new Point(i, 1), new Point(i, 2)))
                     return field.getFigure(new Point(i, 0));
             }
-
             for (int i = 0; i < 3; i++) {
-                if (check(field, new Point(0, i), new IPointChanger() {
-                    @Override
-                    public Point next(Point p) {
-                        return new Point(p.x + 1, p.y);
-                    }
-                } ))
+                if (check(field, new Point(0, i), new Point(1, 0), new Point(2, i)))
                     return field.getFigure(new Point(0, i));
             }
-
-            if (check(field, new Point(0, 0), new IPointChanger() {
-                @Override
-                public Point next(Point p) {
-                    return new Point(p.x + 1, p.y + 1);
-                }
-            } ))
+            if (check(field, new Point(0, 0), new Point(1, 1), new Point(2, 2)))
                 return field.getFigure(new Point(0, 0));
-
-            if (check(field, new Point(0, 2), new IPointChanger() {
-                @Override
-                public Point next(Point p) {
-                    return new Point(p.x + 1, p.y - 1);
-                }
-            } ))
-                return field.getFigure(new Point(0, 0));
+            if (check(field, new Point(0, 2), new Point(1, 1), new Point(2, 0)))
+                return field.getFigure(new Point(1, 1));
 
         } catch (InvalidPointException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    private boolean check(final Field field, final Point startPoint, final IPointChanger pointChanger) {
-
-        final Point p1 = startPoint;
-        final Point p2 = pointChanger.next(p1);
-        final Point p3 = pointChanger.next(p2);
+    private boolean check(final Field field, final Point p1, final Point p2, final Point p3) {
 
         try {
             if (field.getFigure(p1) == null) return false;
